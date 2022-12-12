d9d4c4e9a8c890bcdd1dc6b5bd9d2b9321e03fa7
Anurag Rana <anurag.rana@Anurags-MacBook-Pro.local>
2022-12-12 13:52:11 +0530

    Add solution to AOC 11

---
+++ AOC-Day-11/Monkey.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Numerics;
+
+namespace AOC_Day_11
+{
+    public class Monkey
+    {
+        List<long> items;
+        long divisor;
+        char action;
+        long operand;
+        Monkey trueMonkey;
+        Monkey falseMonkey;
+        bool self;
+        long inspected;
+        long gcd;
+
+        public Monkey(long[] arr, long div, char act, long op, bool isSelfOperand, long g)
+        {
+            items = new List<long>(arr);
+            divisor = div;
+            action = act;
+            operand = op;
+            self = isSelfOperand;
+            inspected = 0;
+            gcd = g;
+        }
+
+        public Monkey SetTrueConditionMonkey
+        {
+            set { this.trueMonkey = value; }
+        }
+
+        public Monkey SetFalseConditionMonkey
+        {
+            set { this.falseMonkey = value; }
+        }
+
+        private void SendIfFalse(long item)
+        {
+            falseMonkey.Items.Add(item);
+        }
+
+        private void SendIfTrue(long item)
+        {
+            trueMonkey.Items.Add(item);
+        }
+
+        public List<long> Items
+        {
+            get { return this.items; }
+        }
+
+        public long Inspected
+        {
+            get { return this.inspected; }
+        }
+
+        private void TestClause(long val)
+        {
+            if (val % divisor == 0)
+            {
+                SendIfTrue(val);
+            }
+            else
+            {
+                SendIfFalse(val);
+            }
+        }
+
+        public void PerformAction()
+        {
+            foreach (long item in items)
+            {
+                long temp = Calculate(item) % gcd;
+                
+                TestClause(temp);
+                inspected++;
+            }
+
+            items.Clear();
+        }
+
+        private long Calculate(long val)
+        {
+            if (self)
+            {
+                operand = val;
+            }
+
+            switch (action)
+            {
+                case '+':
+                    return val + operand;
+                case '-':
+                    return val - operand;
+                case '*':
+                    return val * operand;
+                case '/':
+                    return val / operand;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
+
---
+++ AOC-Day-11/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace AOC_Day_11;
+
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long gcd = 17 * 7 * 13 * 2 * 19 * 3 * 5 * 11;
+
+        Monkey zero = new Monkey(new long[] { 89, 74 }, 17, '*', 5, false, gcd);
+        Monkey one = new Monkey(new long[] { 75, 69, 87, 57, 84, 90, 66, 50 }, 7, '+', 3, false, gcd);
+        Monkey two = new Monkey(new long[] { 55 }, 13, '+', 7, false, gcd);
+        Monkey three = new Monkey(new long[] { 69, 82, 69, 56, 68 }, 2, '+', 5, false, gcd);
+        Monkey four = new Monkey(new long[] { 72, 97, 50 }, 19, '+', 2, false, gcd);
+        Monkey five = new Monkey(new long[] { 90, 84, 56, 92, 91, 91 }, 3, '*', 19, false, gcd);
+        Monkey six = new Monkey(new long[] { 63, 93, 55, 53 }, 5, '*', int.MinValue, true, gcd);
+        Monkey seven = new Monkey(new long[] { 50, 61, 52, 58, 86, 68, 97 }, 11, '+', 4, false, gcd);
+
+        zero.SetTrueConditionMonkey = four;
+        zero.SetFalseConditionMonkey = seven;
+
+        one.SetTrueConditionMonkey = three;
+        one.SetFalseConditionMonkey = two;
+
+        two.SetTrueConditionMonkey = zero;
+        two.SetFalseConditionMonkey = seven;
+
+        three.SetTrueConditionMonkey = zero;
+        three.SetFalseConditionMonkey = two;
+
+        four.SetTrueConditionMonkey = six;
+        four.SetFalseConditionMonkey = five;
+
+        five.SetTrueConditionMonkey = six;
+        five.SetFalseConditionMonkey = one;
+
+        six.SetTrueConditionMonkey = three;
+        six.SetFalseConditionMonkey = one;
+
+        seven.SetTrueConditionMonkey = five;
+        seven.SetFalseConditionMonkey = four;
+
+
+        for (int i = 0; i < 10000; i++)
+        {
+            zero.PerformAction();
+            one.PerformAction();
+            two.PerformAction();
+            three.PerformAction();
+            four.PerformAction();
+            five.PerformAction();
+            six.PerformAction();
+            seven.PerformAction();
+        }
+
+        List<long> inspectedItems = new List<long>();
+
+        inspectedItems.Add(zero.Inspected);
+        inspectedItems.Add(one.Inspected);
+        inspectedItems.Add(two.Inspected);
+        inspectedItems.Add(three.Inspected);
+        inspectedItems.Add(four.Inspected);
+        inspectedItems.Add(five.Inspected);
+        inspectedItems.Add(six.Inspected);
+        inspectedItems.Add(seven.Inspected);
+
+        inspectedItems.Sort();
+
+        BigInteger mul = BigInteger.Multiply(BigInteger.Parse(inspectedItems[inspectedItems.Count - 1].ToString()), BigInteger.Parse(inspectedItems[inspectedItems.Count - 2].ToString()));
+
+        Console.WriteLine(mul.ToString());
+    }
+}
